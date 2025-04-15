737dc2e45a957480da723a064b9df14c56a8b9b4
Danilo Curvelo <danilocurvelo@gmail.com>
2025-04-15 14:34:37 +0000

    fix: fib

--- recursion/fibonacci/main.cpp
+++ recursion/fibonacci/main.cpp
@@ -2,7 +2,7 @@
 
 int fibonacci(int n) {
 
-    if ( (n == 0) || (n == 1) ) {
+    if ( n == 0 || n == 1 ) {
         return n;
     } else {
         return ( fibonacci(n - 1) + fibonacci(n - 2) );
@@ -11,17 +11,25 @@ int fibonacci(int n) {
 }
 
 int main() {
-
     int n;
     std::cout << "Entre com o valor de N: ";
     std::cin >> n; 
 
-    int i = 0;
-    while (i < n) {
-        std::cout << fibonacci(i) << " ";
-        i++;
-    }
-
-    return 0;
-
+    std::cout << "fibonacci(" << n << ") = " <<  fibonacci(n) << " " << std::endl;
 }
+
+// int main() {
+
+//     int n;
+//     std::cout << "Quantos elementos da sequência de Fibonacci você gostaria: ";
+//     std::cin >> n; 
+
+//     int i = 0;
+//     while (i < n) {
+//         std::cout << fibonacci(i) << " ";
+//         i++;
+//     }
+
+//     return 0;
+
+// }
